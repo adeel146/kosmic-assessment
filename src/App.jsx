c450cd3f044c50df33c7904b47fadf426f8c8375
Adeel <mohammad.adeel@graffitecs.com>
2023-11-18 00:18:39 +0500

    created structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { Typography } from "@mui/material";
-import "./App.css";
+// App.jsx
+
+import React from "react";
+import Header from "./components/Header";
+import MainArea from "./components/MainArea";
+import Footer from "./components/Footer";
+import { Grid } from "@mui/material";
 
 function App() {
   return (
-    <>
-      <Typography variant="h1">Hello, World!</Typography>
-    </>
+    <Grid container direction="column" style={{ minHeight: "100vh" }}>
+      {/* Header at the top */}
+      <Grid item style={{ height: "111px" }}>
+        <Header />
+      </Grid>
+
+      {/* MainArea in the middle, takes remaining vertical space */}
+      <Grid item xs flexGrow={1}>
+        <MainArea />
+      </Grid>
+
+      {/* Footer at the bottom with a fixed height */}
+      <Grid item style={{ height: "116px" }}>
+        <Footer />
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", mx: "20px" }}>
+      <Button sx={{ color: "#FFFFFF" }}>Back</Button>
+      <Box
+        sx={{ display: "flex", justifyContent: "space-between", mx: "20px" }}
+      >
+        <Button sx={{ backgroundColor: "#FFFFFF" }}>
+          <Typography
+            sx={{
+              background: "linear-gradient(90deg, #523FD7 100%, #FF7DFF 100%)",
+              WebkitBackgroundClip: "text",
+              color: "transparent",
+            }}
+          >
+            Next
+          </Typography>
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
