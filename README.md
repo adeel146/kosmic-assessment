2d65ed3d8a631aac55ca251049d482dca72f13df
Adeel <mohammad.adeel@graffitecs.com>
2023-11-18 18:18:32 +0500

    add rwadme file

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
-# React + Vite
+# Kosmic Assessment
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Welcome to the Kosmic Assessment project!
 
-Currently, two official plugins are available:
+## Table of Contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+  - [Running the Project](#running-the-project)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Acknowledgments](#acknowledgments)
+
+## Getting Started
+
+These instructions will help you set up and run the project on your local machine.
+
+### Prerequisites
+
+Make sure you have the following software installed on your machine:
+
+- [Node.js](https://nodejs.org/) (version x.x.x)
+- [npm](https://www.npmjs.com/) or [Yarn](https://yarnpkg.com/)
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/adeel146/kosmic-assessment.git
+   ```
--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Autocomplete,
   Chip,
@@ -6,65 +7,190 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
+import styled from "@emotion/styled";
 
 const MainArea = () => {
-  const top100Films = [
-    "Board games revival",
-    "Comedy trends",
-    "Amusement parks",
-    "Escape rooms",
-    "Street performances",
-    "Online challenges",
-    "Festivals impact",
-    "Crafting movement",
-    "Social gaming",
-    "Virtual reality",
+  const contentTypes = [
+    {
+      label: "Fun",
+      options: [
+        "Board games revival",
+        "Comedy trends",
+        "Amusement parks",
+        "Escape rooms",
+        "Street performances",
+        "Online challenges",
+        "Festivals impact",
+        "Crafting movement",
+        "Social gaming",
+        "Virtual reality",
+      ],
+    },
+    {
+      label: "Education",
+      options: [
+        "Gamified learning",
+        "Education systems",
+        "Bilingual benefits",
+        "Standardized testing",
+        "Arts in schools",
+        "Tech bootcamps",
+        "Homeschooling dynamics",
+        "Mentorship importance",
+        "Educational equity",
+        "Mobile learning",
+      ],
+    },
+    {
+      label: "Sports",
+      options: [
+        "Sports medicine",
+        "Team sociology",
+        "Olympic economics",
+        "Gender parity",
+        "Fandom psychology",
+        "Extreme sports",
+        "Sports business",
+        "Youth athletics",
+        "Data analytics",
+        "Doping ethics",
+      ],
+    },
+    {
+      label: "News",
+      options: [
+        "Citizen journalism",
+        "Whistleblower influence",
+        "Fake news",
+        "Print media survival",
+        "Social media news",
+        "Cybersecurity news",
+        "Democratic media",
+        "News deserts",
+        "News economics",
+        "Broadcast evolution",
+      ],
+    },
+    {
+      label: "Investment",
+      options: [
+        "Robo-advising",
+        "Geopolitical impacts",
+        "Index funds",
+        "Real estate trends",
+        "Startup investing",
+        "Social trading",
+        "Tax strategies",
+        "Emerging markets",
+        "Financial literacy",
+        "Pension futures",
+      ],
+    },
+    {
+      label: "Facts",
+      options: [
+        "Animal oddities",
+        "Everyday science",
+        "Historical obscurities",
+        "Science myths",
+        "Human body facts",
+        "Oceanic discoveries",
+        "Space exploration",
+        "Ancient tech",
+        "World laws",
+        "Bizarre news",
+      ],
+    },
   ];
+
+  // Define a custom styled component for the linear gradient slider
+  const CustomSlider = styled(Slider)(({ theme }) => ({
+    color: theme.palette.primary.main,
+    "& .MuiSlider-rail": {
+      backgroundColor: theme.palette.secondary.main, // Adjust as needed
+    },
+    "& .MuiSlider-thumb": {
+      backgroundColor: theme.palette.primary.main,
+      "&:hover, &.Mui-focusVisible": {
+        boxShadow: "0px 0px 0px 8px rgba(255, 125, 255, 0.16)",
+      },
+    },
+    "& .MuiSlider-valueLabel": {
+      color: theme.palette.primary.main,
+    },
+    "& .MuiSlider-valueLabelCircle": {
+      backgroundColor: theme.palette.primary.main,
+    },
+  }));
+
+  const [selectedContent, setSelectedContent] = useState("Fun");
+  const [selectedTitle, setSelectedTitle] = useState(null);
+  const [wordCount, setWordCount] = useState(700);
+
+  const handleContentChange = (content) => {
+    setSelectedContent(content);
+    setSelectedTitle(null); // Reset selected title when content type changes
+  };
+
+  const handleTitleChange = (title) => {
+    setSelectedTitle(title);
+  };
+
   return (
     <div>
       <Typography>Content type</Typography>
       <Typography>Choose a content type that best fits your needs.</Typography>
-      <Typography>What type of content are you creating?</Typography>
       <Stack direction="row" spacing={1}>
-        <Chip
-          label="primary"
-          variant="outlined"
-          sx={{ backgroundColor: "pink" }}
-          onClick={() => console.log("")}
-        />
-      </Stack>
-      <Typography>
-        Which type of “Education” content do you want to create?
-      </Typography>
-      <Autocomplete
-        variant=""
-        size="small"
-        disablePortal
-        id="combo-box-demo"
-        options={top100Films}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField
+        {contentTypes.map((content) => (
+          <Chip
+            key={content.label}
+            label={content.label}
+            variant={selectedContent === content.label ? "filled" : "outlined"}
             sx={{
-              borderRadius: "12px",
-              background: "rgba(58, 57, 64, 0.30)",
-              border: "none",
+              backgroundColor:
+                selectedContent === content.label ? "#523FD7" : "",
             }}
-            variant="outlined"
-            {...params}
-            label="Select"
+            onClick={() => handleContentChange(content.label)}
           />
-        )}
-      />
+        ))}
+      </Stack>
+
+      {selectedContent && (
+        <>
+          <Typography>{`What should be the title for the ”${selectedContent}” discussion?`}</Typography>
+          <Autocomplete
+            size="small"
+            disablePortal
+            id="combo-box-demo"
+            options={
+              contentTypes.find((content) => content.label === selectedContent)
+                .options
+            }
+            value={selectedTitle}
+            onChange={(event, newValue) => handleTitleChange(newValue)}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField
+                sx={{
+                  borderRadius: "12px",
+                  background: "rgba(58, 57, 64, 0.30)",
+                  border: "none",
+                }}
+                variant="outlined"
+                {...params}
+                label="Select"
+              />
+            )}
+          />
+        </>
+      )}
+
       <Typography>Set the number of words for output text.</Typography>
-      <Slider
-        defaultValue={700}
-        // value={value}
+      <CustomSlider
+        value={wordCount}
         min={100}
         max={1000}
-        step={null}
-        valueLabelDisplay="auto"
+        valueLabelDisplay="on"
         track="inverted"
         railProps={{
           sx: {
@@ -81,7 +207,7 @@ const MainArea = () => {
             backgroundColor: "#222",
           },
         }}
-        // onChange={handleChange}
+        onChange={(event, newValue) => setWordCount(newValue)}
       />
     </div>
   );
