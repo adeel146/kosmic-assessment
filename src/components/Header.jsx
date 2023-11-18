ff5eb2ab77565e244a781178f00d0c57b4e5c4c2
Adeel <mohammad.adeel@graffitecs.com>
2023-11-18 17:56:49 +0500

    create layout of the design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import EditIcon from "../assets/icons/EditIcon";
+
+const settings = ["Profile", "Account", "Logout"];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-export default Header
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  const textStyle = {
+    color: "var(--color-onboarding-primary-text, #FFF)",
+    fontFamily: "Poppins",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "normal",
+  };
+
+  const subHeading = {
+    color: "var(--color-onboarding-secondary-text, #D0CFD1)",
+    fontFamily: "Poppins",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "normal",
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Container
+          maxWidth="xl"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            paddingTop: "12px",
+            paddingBottom: "12px",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <IconButton>
+              <EditIcon />
+            </IconButton>
+            <Box
+              sx={{
+                display: "flex",
+                flexFlow: "column",
+              }}
+            >
+              <Typography style={textStyle}>
+                Media management &nbsp; <EditIcon />{" "}
+              </Typography>
+              <Typography style={subHeading}>Draft Compaign</Typography>
+            </Box>
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Container>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
--- src/components/MainArea.jsx
+++ src/components/MainArea.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import {
+  Autocomplete,
+  Chip,
+  Slider,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 const MainArea = () => {
+  const top100Films = [
+    "Board games revival",
+    "Comedy trends",
+    "Amusement parks",
+    "Escape rooms",
+    "Street performances",
+    "Online challenges",
+    "Festivals impact",
+    "Crafting movement",
+    "Social gaming",
+    "Virtual reality",
+  ];
   return (
-    <div>MainArea</div>
-  )
-}
+    <div>
+      <Typography>Content type</Typography>
+      <Typography>Choose a content type that best fits your needs.</Typography>
+      <Typography>What type of content are you creating?</Typography>
+      <Stack direction="row" spacing={1}>
+        <Chip
+          label="primary"
+          variant="outlined"
+          sx={{ backgroundColor: "pink" }}
+          onClick={() => console.log("")}
+        />
+      </Stack>
+      <Typography>
+        Which type of “Education” content do you want to create?
+      </Typography>
+      <Autocomplete
+        variant=""
+        size="small"
+        disablePortal
+        id="combo-box-demo"
+        options={top100Films}
+        sx={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField
+            sx={{
+              borderRadius: "12px",
+              background: "rgba(58, 57, 64, 0.30)",
+              border: "none",
+            }}
+            variant="outlined"
+            {...params}
+            label="Select"
+          />
+        )}
+      />
+      <Typography>Set the number of words for output text.</Typography>
+      <Slider
+        defaultValue={700}
+        // value={value}
+        min={100}
+        max={1000}
+        step={null}
+        valueLabelDisplay="auto"
+        track="inverted"
+        railProps={{
+          sx: {
+            backgroundColor: "#ccc",
+          },
+        }}
+        trackProps={{
+          sx: {
+            backgroundColor: "#777",
+          },
+        }}
+        thumbProps={{
+          sx: {
+            backgroundColor: "#222",
+          },
+        }}
+        // onChange={handleChange}
+      />
+    </div>
+  );
+};
 
-export default MainArea
+export default MainArea;
